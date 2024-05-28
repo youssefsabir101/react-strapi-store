d7802796c033a0bff12d7ce326895b1a749970c5
youssefsabir101 <youssefsabirprog@gmail.com>
2024-05-28 14:46:15 +0100

    update the faq page

--- client/src/components/staticPages/Faq.jsx
+++ client/src/components/staticPages/Faq.jsx
@@ -50,19 +50,7 @@ function Faq() {
 
     return (
       <>
-        <Navbar />
-      
-
-
-
-
-
-
-
-
-
-
-        
+        <Navbar />      
         {/* FAQs */}
         <section className="relative bg-transparent ">
           <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
